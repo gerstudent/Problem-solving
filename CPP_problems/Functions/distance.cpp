6016a3b91cd70a59e8fca0d3eaa0176857f2d871
German <glesmirol@gmail.com>
2022-03-20 22:43:56 +0300

    Adding a solution to the "IsPointSquare" problem

--- CPP_problems/Functions/distance.cpp
+++ CPP_problems/Functions/distance.cpp
@@ -1,22 +1,25 @@
 /*
     Created by gerstudent
-    Task:Given four real numbers: x1, y1, x2, y2.
-    Write a distance function (x1, y1, x2, y2) that calculates
-    the distance between the point (x1. y1) and (x2, y2). Count
-    four real numbers and output the result of this function.
+    Task: Two real numbers x and y are given. Check whether
+    the point with coordinates (x, y) belongs to the shaded
+    square (including its border). If the point belongs to
+    a square, print the word YES, otherwise print the word NO.
+    In the figure, the grid is drawn in step 1.
 */
 
 #include <iostream>
-#include "cmath"
 using namespace std;
 
-double distance(double a, double b, double c, double d){
-    return sqrt(pow(c - a, 2) + pow(d - b, 2));
+bool IsPointSquare(double x, double y) {
+    bool x_sq = (x >= -1) && (x <= 1);
+    bool y_sq = (y >= -1) && (y <= 1);
+    return x_sq && y_sq;
 }
 
 int main() {
-    double a, b, c, d;
-    cin >> a >> b >> c >> d;
-    cout << distance(a, b, c, d);
+    double x, y;
+    cin >> x >> y;
+    if (IsPointSquare(x, y)) cout << "YES";
+    else cout << "NO";
     return 0;
 }
